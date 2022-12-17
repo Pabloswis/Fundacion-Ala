ffed6c69351e50cae17e13f96769175c7be9121a
QuBiit <leomalvarez89@gmail.com>
2022-12-17 17:13:46 -0300

    cambio visual

--- blog/urls.py
+++ blog/urls.py
@@ -24,7 +24,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
 
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name = 'admin'),
     # 1 PARAMETROS ES EL TEXTO DE LA URL
     # 2 LA VISTA QUE VA EJECUTAR
     # 3 ES EL NOMBRE LA URL (aun no lo usamos)
